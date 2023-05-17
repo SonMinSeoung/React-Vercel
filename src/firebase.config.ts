f73bcea8763ab7857dfd13717db9a08aa4e9e98c
SonMinSeoung <dlgkfn4@naver.com>
2023-05-17 15:47:22 +0900

    firebase 설치& 호스팅

---
+++ src/firebase.config.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDTJNftbNywxerRHJYESYlxqW8-XRTdREE",
+  authDomain: "minseoungreact.firebaseapp.com",
+  projectId: "minseoungreact",
+  storageBucket: "minseoungreact.appspot.com",
+  messagingSenderId: "334767008993",
+  appId: "1:334767008993:web:ac242070f878454953dba0",
+  measurementId: "G-F0JGN89WG9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
