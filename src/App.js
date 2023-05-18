21e12dbde9788461eb8c333c30f551c68266fba1
SonMinSeoung <dlgkfn4@naver.com>
2023-05-18 09:02:25 +0900

    부트스트랩이좋은거같아

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Navigation } from 'react-router-dom';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Navigation />
     </div>
   );
 }
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+function App() {
+  return (
+    <Container>
+      <div>dddddddd</div>
+    </Container>
+  );
+}
+
+export default App;
