0b059e810edc08e7e28e0643468830c56f936397
SonMinSeoung <dlgkfn4@naver.com>
2023-05-18 13:31:15 +0900

    워려어

--- src/components/Hoxy.tsx
+++ src/components/Hoxy.tsx
@@ -1,8 +1,31 @@
 import React from "react";
+import { unstable_HistoryRouter } from "react-router-dom";
 
+
+interface messageLinkStyles {
+    display : string;
+    textAlign : string;
+    background : string;
+    textTransform : string;
+    textDecoration : string;
+    color : string;
+    padding : string;
+    borderRadius : string;
+}
 function Hoxy(){
-    return (
-        <div>혹시 이것좀 적어줄수있겟니</div>
-    )
+    const messageLinkStyles: messageLinkStyles = {
+        display: 'block',
+        textAlign: 'center',
+        background: '#039be5',
+        textTransform: 'uppercase',
+        textDecoration: 'none',
+        color: 'white',
+        padding: '16px',
+        borderRadius: '4px',
+      };
+      return(
+        <>
+        </>
+      )
 }
 export default Hoxy;
