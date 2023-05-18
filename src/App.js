c571c88997467df1278c929387ba336ec41819c1
SonMinSeoung <dlgkfn4@naver.com>
2023-05-18 12:41:46 +0900

    ddd

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import  Navigation  from './components/Navigation';
-import { Outlet } from 'react-router-dom';
+import HomePage from './components/HomePage';
+
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Outlet />
+      <HomePage />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ErrorPage from './components/ErrorPage';
 import HomePage from './components/HomePage';
-
-
-const router = createBrowserRouter({
-  path:"/",
-  element:<App />,
-  errorElement: <ErrorPage />,
-  children:[
-    {path:"/", element:<HomePage />}
-  ]
-})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    {router}
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/error" element={<ErrorPage />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
